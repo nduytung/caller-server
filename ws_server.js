dd4251370f9f7e2274a17418310f097f42e6035d
Nguyen Duy Tung <rye@Nguyens-Laptop.local>
2024-11-30 23:49:32 +0700

    fix: refactor

--- ws_server.js
+++ ws_server.js
@@ -1,37 +1,26 @@
-// Import the WebSocket library
 const WebSocket = require("ws");
 
-// WebSocket Server Setup
 const wss = new WebSocket.Server({ port: 8080 });
 
-console.log("Server started on port 8080");
+console.log("Caller server ready on port 8080");
 
-// Map to track client subscriptions
 const clientSubscriptions = new Map();
 
 wss.on("connection", (ws) => {
-  console.log("Client connected.");
+  console.log("New client connected. Total clients: ", wss.clients.size);
 
-  // Initialize an empty set of subscriptions for each client
   clientSubscriptions.set(ws, new Set());
 
-  // Listen for messages from the client
   ws.on("message", (message) => {
-    // Parse the message (assuming it's JSON)
     try {
       const data = JSON.parse(message);
 
-      console.log("received from client: ", data.action);
+      console.log("received from client: ", data.type, data);
 
-      if (data.type === "subscribe" && data.action) {
-        // Add the action to the client's subscription set
+      if (data.type === "subscribe" && data.type && data.action) {
         clientSubscriptions.get(ws).add(data.action);
         console.log(`Client subscribed to: ${data.action}`);
-      } else if (data.type === "unsubscribe" && data.action) {
-        // Remove the action from the client's subscription set
-        clientSubscriptions.get(ws).delete(data.action);
-        console.log(`Client unsubscribed from: ${data.action}`);
-      } else {
+      } else if (data.type !== "subscribe" && data.type) {
         broadcastMessage(data.type, data);
       }
     } catch (error) {
@@ -51,9 +40,10 @@ wss.on("connection", (ws) => {
 
 // Broadcast messages to clients with the given subscription
 function broadcastMessage(action, message) {
-  console.log("broadcasting msg: ", action);
   for (const [client, subscriptions] of clientSubscriptions.entries()) {
+    console.log("subscriptions: ", subscriptions);
     if (subscriptions.has(action) && client.readyState === WebSocket.OPEN) {
+      console.log("found it!, sending");
       client.send(JSON.stringify(message));
     }
   }
@@ -75,10 +65,11 @@ async function listen() {
   websocket.on("message", (message) => {
     try {
       const data = JSON.parse(message);
+
       console.log("received from wss remote: ", data);
-      broadcastMessage("new_token_call", {
-        type: "new_token_call",
-        message: data,
+      broadcastMessage("new_token_call_sig", {
+        type: "new_token_call_sig",
+        analyze: data?.message,
       });
     } catch (error) {
       console.error(`Error parsing message: ${error}`);
